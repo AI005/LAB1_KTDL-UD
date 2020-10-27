a71a32c77942fe9af0a529fa5392ff82adf51282
nguyencongbinhphuong12091999@gmail.com <nguyencongbinhphuong12091999@gmail.com>
2020-10-27 16:25:42 +0700

    function cho chuc nang 3

--- Source/main.py
+++ Source/main.py
@@ -1,6 +1,8 @@
 import csv
+import statistics
+from random import choice
 
-
+filename = 'test.csv'
 # Tra ve list cac key co gia tri rong theo index
 # vi du:
 # + list[0] = {row1, row2}, nghia la tai row1, row2 hang thu 0 mang gia tri rong
@@ -28,17 +30,58 @@ def list_col_less_data(list_missing_data):
 def get_number_of_row_less_data(list_missing_data):
     return len(list_missing_data)
 
+def read_the_csv(input_file):
+    input_file.seek(0)
+    csv_reader = csv.DictReader(input_file)
+    for row in csv_reader:
+        yield row
 
-filename = 'house-prices.csv'
+
+# chuc nang 3
+def get_value_of_col(table, col_name):
+    return [row[col_name] for row in table]
+
+def mean(table, col_name):
+    return statistics.mean([int(x) for x in get_value_of_col(table, col_name) if x != ''])
+
+def median(table, col_name):
+    return statistics.median_grouped([int(x) for x in get_value_of_col(table, col_name) if x != ''])
+
+def mode(table, col_name):
+    return choice(statistics.multimode([int(x) for x in get_value_of_col(table, col_name) if x != '']))
+
+def get_full_col_with_mean(table, col_name):
+    mean_value = str(mean(table, col_name))
+    new_col = list(map(lambda a: a if a != '' else mean_value, get_value_of_col(table, col_name)))
+    return new_col
+
+def get_full_col_with_median(table, col_name):
+    median_value = str(median(table, col_name))
+    new_col = list(map(lambda a: a if a != '' else median_value, get_value_of_col(table, col_name)))
+    return new_col
+
+def get_full_col_with_mode(table, col_name):
+    mode_value = str(mode(table, col_name))
+    new_col = list(map(lambda a: a if a != '' else mode_value, get_value_of_col(table, col_name)))
+    return new_col
+
+
 with open(filename) as file:
-    spamreader = csv.DictReader(file)
-    list_missing_data = get_list_missing_data(spamreader)
-    display_list_missing_data(list_missing_data)
+    spamreader = read_the_csv(file)
+    reader = list(spamreader)
+    list_missing_data = get_list_missing_data(reader)
 
-
+    # display_list_missing_data(list_missing_data)
     #Chuc nang 1
     print('Cac cot cua ')
     print('Cac cot bi thieu du lieu: ' + str(list_col_less_data(list_missing_data)))
     print('So dong bi thieu du lieu: ' + str(get_number_of_row_less_data(list_missing_data)))
+    display_list_missing_data(list_missing_data)
 
-
+    print(get_value_of_col(reader, col_name='sotien'))
+    print(mean(reader, col_name='sotien'))
+    print(median(reader, col_name='sotien'))
+    print(mode(reader, col_name='sotien'))
+    print(get_full_col_with_mean(reader, col_name='sotien'))
+    print(get_full_col_with_median(reader, col_name='sotien'))
+    print(get_full_col_with_mode(reader, col_name='sotien'))
---
+++ Source/test.py
@@ -0,0 +1,6 @@
+from collections import Counter
+from statistics import multimode
+
+a = [1,1,1,2,2,3,3,4,4,4,5]
+
+print(multimode(a))
